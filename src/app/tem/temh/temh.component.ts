64ea5066bb4a51e913472e475302b785f8a9df72
Karla Torres <torrestrujillokarla@gmail.com>
2024-11-08 18:37:46 -0600

    LISTA ALUMNOS 08/11/2024 KARLA TORRES

--- src/app/tem/temh/temh.component.ts
+++ src/app/tem/temh/temh.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-temh',
@@ -9,6 +9,11 @@ import { Component, Input } from '@angular/core';
 })
 export class TemhComponent {
 
-  @Input() mensaje:string=''
+  @Input() mensaje:string='';
+
+  @Output() mensaje2 = new EventEmitter<string>();
+  eventMensaje(){
+    this.mensaje2.emit('Hola desde hijo');
+  }
 
 }
